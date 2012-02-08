54ec337d7f3e2162c8aa6a0b2596e022bb468787
Gea-Suan Lin <darkkiller@gmail.com>
2012-02-08 21:20:22 +0800

    Add condition rule to avoid call null

--- autoload-images-ruten.user.js
+++ autoload-images-ruten.user.js
@@ -3,12 +3,15 @@
 // @namespace   http://blog.gslin.org/plugins/autoload-images-ruten
 // @description Autoload Images in ruten.com.tw
 // @homepage    http://blog.gslin.org/plugins/autoload-images-ruten
-// @version     20110208.0
+// @version     20110208.1
 // @include     http://goods.ruten.com.tw/item/show?*
 // ==/UserScript==
 
 (function(){
-    unsafeWindow.second_stage_image_loader();
+    if (unsafeWindow.second_stage_image_loader) {
+        unsafeWindow.second_stage_image_loader();
+    }
+
     var a = document.getElementById('arrow_detail');
     if (a) {
         a.style.display = 'none';
